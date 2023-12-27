7e1fb0e8e52ddc3b52ce9f6705ef30e05a34a50c
printfCAT <cliffomanyo@gmail.com>
2023-12-27 09:00:29 +0300

    implemented login and signup

--- challonge/src/index.js
+++ challonge/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Signup from './Signup';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Signup />
   </React.StrictMode>
 );
 
