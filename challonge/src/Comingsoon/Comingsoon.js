92e6c1959959b757fefc5cf9c524ef7307a62388
printfCAT <cliffomanyo@gmail.com>
2024-01-02 19:29:06 +0300

    minor fixes

--- challonge/src/Comingsoon/Comingsoon.js
+++ challonge/src/Comingsoon/Comingsoon.js
@@ -4,7 +4,9 @@ function Comingsoon() {
     return (
         <><body>
         <div className="heading">
+            <div className="diagonal-line"></div>
             <h1>COMING SOON!</h1>
+            <div className="diagonal-line1"></div>
         </div>
       </body></>
     )
